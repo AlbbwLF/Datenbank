50f9e1edbfc087e9aa55fd3fed053a2891688e03
AlbbwLF <Lea.Fabian@albbw.de>
2026-03-27 13:17:08 +0100

    Verbindungsdaten.php erstellt und die Verbindung zum Server generiert

--- datenbank.php
+++ datenbank.php
@@ -1,15 +1,16 @@
 <?php 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "randomtables";
-
-//Creating connection
-$conn = mysqli_connect($localhost, $username, $password, $randomtables);
-
-//Check connection
-if(!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+require_once ('Verbindungsdaten.php');
+$db_link = mysqli_connect (
+    $datenbank_host,
+    $datenbank_user, 
+    $datenbank_passwort,
+    $datenbank_name
+);
+if ($db_link){
+    echo 'Verbindung erfolgreich: ';
+    print_r($db_link);
+} 
+else {
+    die('Keine Verbindung möglich: ' .mysqli_error());
+    }
 ?>
