35d2313c39fc5d7b42a9f5ee15d30f30c6ffeffb
AlbbwLF <Lea.Fabian@albbw.de>
2026-03-27 13:52:01 +0100

    Tabelle aufgerufen

--- datenbank.php
+++ datenbank.php
@@ -1,16 +1,27 @@
 <?php 
 require_once ('Verbindungsdaten.php');
-$db_link = mysqli_connect (
-    $datenbank_host,
-    $datenbank_user, 
-    $datenbank_passwort,
-    $datenbank_name
-);
-if ($db_link){
-    echo 'Verbindung erfolgreich: ';
-    print_r($db_link);
-} 
-else {
-    die('Keine Verbindung möglich: ' .mysqli_error());
+
+//Create Connection
+$conn = new mysqli($datenbank_host, $datenbank_user, $datenbank_passwort, $datenbank_name);
+
+//Check Connection
+if ($conn->connect_error) {
+    die('Verbindung fehlgeschlagen: '. $conn->connect_error);
+}
+
+$sql = "select KdNr, Nachname, Vorname from kundennummer";
+//Executing the SQL query
+$result = $conn->query($sql);
+
+//Processing the result set
+if ($result->num_rows > 0) {
+    //Outputting the data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo"KdNr: ". $row["KdNr"] ." Name: ". $row["Nachname"]." Vorname: ". $row["Vorname"]. "<br>";
     }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
