2243dc6bd5ebf4fe3c99e2933980b78eb846bb60
AlbbwLF <Lea.Fabian@albbw.de>
2026-03-27 13:07:54 +0100

    Verbindungsdaten php erstellt

---
+++ Verbindungsdaten.php
@@ -0,0 +1,6 @@
+<?php
+$datenbank_name = 'randomtables';
+$datenbank_host = 'localhost';
+$datenbank_user = 'root';
+$datenbank_passwort = '';
+?> 
--- datenbank.php
+++ datenbank.php
@@ -1,2 +1,15 @@
-<?php echo 'Test';?>
+<?php 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "randomtables";
 
+//Creating connection
+$conn = mysqli_connect($localhost, $username, $password, $randomtables);
+
+//Check connection
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
