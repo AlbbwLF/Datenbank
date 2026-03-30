38d94b30e477bcd25557c2dc117afee87754ea7d
AlbbwLF <Lea.Fabian@albbw.de>
2026-03-30 13:20:28 +0200

    tabelle.php ausgefüllt

--- tabelle.php
+++ tabelle.php
@@ -1,1 +1,18 @@
 <?php
+echo "<h1>Kundendaten</h1>";
+
+if(!empty($kunden)){
+    echo "<table class='kunden-tabelle'>";
+    echo"<tr><th>KdNr</th><th>Nachname</th><th>Vorname</th></tr>";
+
+    foreach($kunden as $row) {
+        echo "<tr>";
+        echo "<td>{$row['KdNr']}</td>";
+        echo "<td>{$row['Nachname']}</td>";
+        echo "<td>{$row['Vorname']}</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Keine Einträge gefunden.";
+}
