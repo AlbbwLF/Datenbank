fece4c07a733797aa7ebc9d1d5478152de96d8fe
AlbbwLF <Lea.Fabian@albbw.de>
2026-03-30 11:57:05 +0200

    Added design.css und html table

--- datenbank.php
+++ datenbank.php
@@ -9,18 +9,26 @@ if ($conn->connect_error) {
     die('Verbindung fehlgeschlagen: '. $conn->connect_error);
 }
 
+echo "<h1>Kundendaten</h1>";
+
 $sql = "select KdNr, Nachname, Vorname from kundennummer";
 //Executing the SQL query
 $result = $conn->query($sql);
 
 //Processing the result set
 if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>KdNr</th><th>Nachname</th><th>Vorname</th></tr>";
     //Outputting the data of each row
     while ($row = $result->fetch_assoc()) {
-        echo"KdNr: ". $row["KdNr"] ." Name: ". $row["Nachname"]." Vorname: ". $row["Vorname"]. "<br>";
+        echo "<tr>";
+        echo "<td>".$row["KdNr"]."</td>";
+        echo "<td>".$row["Nachname"]."</td>";
+        echo "<td>".$row["Vorname"]."</td>";
     }
+    echo "</table>";
 } else {
-    echo "0 results";
+    echo "Keine Einträge gefunden.";
 }
 
 $conn->close();
