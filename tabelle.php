dced705e9095f6473972828820fd476ce3ec667c
AlbbwLF <Lea.Fabian@albbw.de>
2026-04-24 09:52:29 +0200

    bei der "Neuer Eintrag" Tabelle das Header auf h1 geändert

--- tabelle.php
+++ tabelle.php
@@ -36,7 +36,7 @@ if(!empty($kunden)){
     echo "</table>";
     echo "<br>";
     echo "<tr>";
-    echo "<h2>Neuer Eintrag</h2>";
+    echo "<h1>Neuer Eintrag</h1>";
     echo "<br>";
     echo "<td><input type='text' name='Nachname[new]' ></td>";
     echo "<td><input type='text' name='Vorname[new]' ></td>";
